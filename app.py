c0861c0c175db6f13a8cf024659d867cc9dfa4d4
cxrvxxxx <brentempasisx3@gmail.com>
2024-05-24 23:22:54 +0800

    added is_valid to response data.

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-import pathlib
-import textwrap
-import threading
+from __future__ import annotations
+
 import os
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 
 import google.generativeai as genai
+
 api_key = os.environ['API_KEY']
 if not os.path.exists('.env') or not api_key:
     with open('.env', 'w') as f:
@@ -14,7 +14,8 @@ if not os.path.exists('.env') or not api_key:
 
     print("Check the .env file and add the API_KEY")
     exit()
-genai.configure(api_key=os.environ['API_KEY'])
+else:
+    genai.configure(api_key=os.environ['API_KEY'])
 
 app = Flask(__name__)
 cors = CORS(
@@ -28,20 +29,33 @@ cors = CORS(
 
 model = genai.GenerativeModel('gemini-pro')
 
+def preprocess(query: str) -> bool:
+    base_prompt = "Yes or No. Does this statement say anything about possible illness symptoms?"
+    response = model.generate_content(f"{base_prompt}: '{query}")    
+
+    return 'yes' in response.text.lower()
+
 @app.route("/ask", methods=["POST"])
-def ask():
+def ask() -> Response:
     base_prompt = "In one sentence, tell me what possible illness I have given the following symptoms: "
     if request.method == "POST":
         query = request.json.get('query')
         message = None
 
-        if not query:
+        is_valid = preprocess(query)
+
+        if not is_valid:
             message = "Please enter valid symptooms."
         else:
             response = model.generate_content(base_prompt + query)
             message = response.text
 
-        return jsonify({"message": message}), 200
+        data = {
+            "is_valid": is_valid,
+            "message": message
+        }
+
+        return jsonify(data), 200
 
 if __name__ == "__main__":
     app.run()
